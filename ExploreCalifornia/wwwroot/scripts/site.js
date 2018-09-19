fd4a6d45152d98a8255617dd42c65a17d27f1685
Christian Schou <chri314x@edu.eal.dk>
2018-09-19 21:35:53 +0200

    Added jquery for AJAX use on blog pages. This includes a _ViewStart because we refresh a partialView inside the page with Ajax.

---
+++ ExploreCalifornia/wwwroot/scripts/site.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    // Whenever any link is clicked in the navigation section
+    // of the blog page, request that content via AJAX
+    $('#mainContent').on('click', '.pager a', function() {
+        var url = $(this).attr('href');
+
+        $('#mainContent').load(url);
+
+        return false;
+    })
+})
